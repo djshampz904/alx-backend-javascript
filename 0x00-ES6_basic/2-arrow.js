5003603996b8bb63f9e3fea9920b04213a16c5d5
djshampz904 <martin.kimani@strathmore.edu>
2024-07-29 17:54:20 +0300

    back end 0-constants.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,7 +1,6 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const self = this; // eslint-disable-line no-unused-vars
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
