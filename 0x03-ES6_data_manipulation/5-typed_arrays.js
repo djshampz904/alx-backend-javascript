883b0e06455258c62e485a5e75678f8b9a2afbd4
djshampz904 <martin.kimani@strathmore.edu>
2024-07-30 15:34:32 +0300

    back end 0-constants.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-    if (position >= length) {
-        throw new Error('Position outside range');
-    }
-    const view = new DataView(new ArrayBuffer(length), 0, length);
-    view.setInt8(position, value);
-    return view;
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const view = new DataView(new ArrayBuffer(length), 0, length);
+  view.setInt8(position, value);
+  return view;
 }
