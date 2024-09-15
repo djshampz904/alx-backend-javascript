eefb3930321e72bdc8a319bb308ef0ed0e1986b2
martin-maverick <97390728+djshampz904@users.noreply.github.com>
2024-09-15 13:54:51 +0300

    Add files via upload

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,16 @@
-const getListStudentIds = (students) => {
-  if (!Array.isArray(students)) {
-    return [];
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
   }
-  return students.map((student) => student.id);
-};
-
-export default getListStudentIds;
+  return [];
+}
